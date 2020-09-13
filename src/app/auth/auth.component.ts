e1f90b273c5c50ff77c1258e929500a8b5e88227
Chinmay Kulkarni <30969403+Grandolf49@users.noreply.github.com>
2020-09-13 21:06:52 +0530

    Added AuthComponent

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,45 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+export class AuthComponent implements OnInit {
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+  }
+
+  isAuthenticated() {
+    let obs = this.http.get('http://localhost:8080/auth');
+    obs.subscribe((response) => {
+      console.log(response)
+    })
+  }
+
+  login(username: String, password: String) {
+    const credentials = {
+      'username': username,
+      'password': password
+    }
+    const config = { headers: new HttpHeaders().set('Content-Type', 'application/json') };
+
+    let obs = this.http.post('http://localhost:8080/login', credentials, config);
+    obs.subscribe((response) => {
+      console.log(response)
+    });
+  }
+
+  createPassword(username: String, password: String) {
+    let obs = this.http.post('http://localhost:8080/login', {
+      'username': username,
+      'password': password
+    });
+    obs.subscribe((response) => {
+      console.log(response)
+    })
+  }
+}
